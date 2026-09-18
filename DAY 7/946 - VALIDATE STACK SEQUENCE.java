417057211a3642d67f7e7cf2691091dc93dc38bf
Tarun <tarunar006@gmail.com>
2026-09-18 08:52:30 +0530

    Implement stack sequence validation algorithm

---
+++ DAY 7/946 - VALIDATE STACK SEQUENCE.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+
+        Stack<Integer> stack = new Stack<>();
+        int j = 0;
+
+        for (int num : pushed) {
+
+            stack.push(num);
+
+            while (!stack.isEmpty() && stack.peek() == popped[j]) {
+                stack.pop();
+                j++;
+
+                if (j == popped.length) {
+                    break;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
