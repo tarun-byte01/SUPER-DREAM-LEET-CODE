854ded47819cc06575a158904aa77e6182fd0296
Tarun <tarunar006@gmail.com>
2026-09-22 17:46:29 +0530

    Implement diameter calculation for binary tree

---
+++ DAY 12/543 - DIAMETER OF BINARY TREE.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    int diameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        height(root);
+        return diameter;
+    }
+
+    private int height(TreeNode node) {
+
+        if (node == null) {
+            return 0;
+        }
+
+        int left = height(node.left);
+        int right = height(node.right);
+
+        diameter = Math.max(diameter, left + right);
+
+        return Math.max(left, right) + 1;
+    }
+}
